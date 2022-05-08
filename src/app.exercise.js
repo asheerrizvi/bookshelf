22b6ee50d55ccd1662c6ad16366cc807eccfb93d
Asheer Rizvi <asheer.vil@gmail.com>
2022-05-08 15:01:23 +0530

    Exercise 04: Extra Credit 1.

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -2,24 +2,29 @@
 import {jsx} from '@emotion/core'
 
 import * as React from 'react'
-// 🐨 you're going to need this:
 import * as auth from 'auth-provider'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
+import { client } from 'utils/api-client.exercise'
+
+async function getUser() {
+  let user = null
+
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+
+  return user
+}
 
 function App() {
-  // 🐨 useState for the user
-
-  // 🐨 create a login function that calls auth.login then sets the user
-  // 💰 const login = form => auth.login(form).then(u => setUser(u))
-  // 🐨 create a registration function that does the same as login except for register
-
-  // 🐨 create a logout function that calls auth.logout() and sets the user to null
-
-  // 🐨 if there's a user, then render the AuthenticatedApp with the user and logout
-  // 🐨 if there's not a user, then render the UnauthenticatedApp with login and register
-
   const [user, setUser] = React.useState(null)
+
+  React.useEffect(() => {
+    getUser().then(u => setUser(u))
+  }, [])
 
   function login(form) {
     return auth.login(form).then(u => setUser(u))
