f7fbdda8251be21f53f5c3b2572790254c2b0a92
Asheer Rizvi <asheer.vil@gmail.com>
2022-05-17 17:39:34 +0530

    Exercise 07: Context

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -1,2 +1,5 @@
+import * as React from 'react'
 // 🐨 create and export a React context variable for the AuthContext
 // 💰 using React.createContext
+const AuthContext = React.createContext()
+export {AuthContext}
