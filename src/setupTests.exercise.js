904284327b18f01db2e15e5f1dc9a7e7a5864071
Asheer Rizvi <asheer.vil@gmail.com>
2022-06-06 22:13:36 +0530

    Exercise 13: Extra Credit 4.

--- src/setupTests.exercise.js
+++ src/setupTests.exercise.js
@@ -1,4 +1,9 @@
 import '@testing-library/jest-dom'
+import * as auth from 'auth-provider'
+import {queryCache} from 'react-query/dist/react-query.development'
+import * as booksDB from 'test/data/books'
+import * as listItemsDB from 'test/data/list-items'
+import * as usersDB from 'test/data/users'
 import {server} from 'test/server'
 
 // enable API mocking in test runs using the same request handlers
@@ -6,3 +11,13 @@ import {server} from 'test/server'
 beforeAll(() => server.listen())
 afterAll(() => server.close())
 afterEach(() => server.resetHandlers())
+
+afterEach(async () => {
+  queryCache.clear()
+  await Promise.all([
+    await auth.logout(),
+    await usersDB.reset(),
+    await booksDB.reset(),
+    await listItemsDB.reset(),
+  ])
+})
