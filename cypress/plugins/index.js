16a9306f88c2c10a9beb42755ed03837956df262
Asheer Rizvi <asheer.vil@gmail.com>
2022-06-09 16:18:07 +0530

    Exercise 15: e2e testing.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -3,7 +3,7 @@ const isCI = require('is-ci')
 module.exports = (on, config) => {
   const isDev = config.watchForFileChanges
   if (!isCI) {
-    config.baseUrl = isDev ? 'http://localhost:3000' : 'http://localhost:8811'
+    config.baseUrl = isDev ? 'http://localhost:3001' : 'http://localhost:8811'
   }
   Object.assign(config, {
     integrationFolder: 'cypress/e2e',
